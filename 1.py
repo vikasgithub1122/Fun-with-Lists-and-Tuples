587c09d6a0788f1a897b67c98e04931687d63e7c
vikasgithub1122 <109681282+vikasgithub1122@users.noreply.github.com>
2023-02-27 19:58:12 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,16 @@
-tuple = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 
 List2 =[]
 List3 =[]
 
-for i in tuple:
+for i in list:
     List2.append(i[1],)
 
 List2.sort()
 #print(List2)
 
 for j in List2:
-    for q in tuple:
+    for q in list:
         if j == int(q[1],):
             List3.append(q)
 
